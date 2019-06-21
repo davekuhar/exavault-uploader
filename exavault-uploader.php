4c9e789705546e122d8295b9e8cdf457dfc7d8dc
Transmit Studio <1170152+transmitstudio@users.noreply.github.com>
2019-06-21 16:22:32 -0700

    added files via upload

---
+++ exavault-uploader.php
@@ -0,0 +1,56 @@
+<?php
+  /**
+  * Plugin Name: Exavault Uploader
+  * Plugin URI: 
+  * Description: Exavault service to upload files.
+  * Version: 1.0
+  * Author: Magmalabs
+  * Author URI:
+  * License: MIT
+  */
+  
+  require_once('V1Api.php');
+  require_once('APIClient.php');
+
+  function add_exavault_compress_api() {
+    register_rest_route('exavault/v1', '/compress', array(
+      'methods' => 'POST',
+      'callback' => 'compress_exavault_files'
+    ));
+  }
+
+  function compress_exavault_files() {
+    $apiKey = 'goldenstaterepro-c6L6JqZao64819bf5AYt2';
+    $apiUrl = 'https://api.exavault.com';
+    $username = 'goldenstaterepro';
+    $password = '$$1020Commercial103';
+
+    
+    $filesPath = $_POST['filePaths'];
+    $archivePath = $_POST['archivePath'];
+
+    // create a new instance of the ExaVault API library class
+    $api = new V1Api( new APIClient($apiKey, $apiUrl) );
+    $accessToken = null;
+
+    // call the authenticateUser method, passing your username and password
+    $response = $api->authenticateUser($username, $password);
+
+    // save this result for later, we will need it to logout
+    $loginSuccess = $response->success;
+
+    // if authentication was successful, store the access token
+    // obtained via the response body in the API instance.
+    if ($loginSuccess) {
+      $accessToken = $response->results->accessToken;
+      $api->compressFiles($accessToken, $filesPath, $archivePath);
+
+      $api->logoutUser($accessToken);
+    }
+
+    header("HTTP/1.1 200 OK");
+  }
+
+  add_action('rest_api_init', 'add_exavault_compress_api');
+?>
+
